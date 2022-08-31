76048f0b14b6ea618449caacc324fd2adfb13c6d
generalpeanuts <dragneel123@gmail.com>
2022-08-31 23:29:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
 import urllib
 import asyncio
 import random
